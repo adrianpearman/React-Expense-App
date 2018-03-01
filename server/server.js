73e84d979080e98af3dabc926165a92251203697
Adrian Pearman <adrianpearman12@gmail.com>
2018-03-01 17:25:12 -0500

    updating server/server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const publicPath = path.join(__dirname, '..', 'public')
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 
