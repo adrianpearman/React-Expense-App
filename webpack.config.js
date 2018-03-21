7f9a7782f8c9514a682d85cbd26ea452f0eb25ce
Adrian Pearman <adrianpearman12@gmail.com>
2018-03-21 00:40:52 -0400

    more bug fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 publicPath = path.join(__dirname, 'public', 'dist')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
